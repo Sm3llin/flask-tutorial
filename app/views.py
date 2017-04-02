a5378d3748eb924c0bd7c6d131e3d6ada98d0a79
Sm3llin <michaelellin@gmail.com>
2017-04-03 00:43:45 +1000

    Added base html template and added posts to page

--- app/views.py
+++ app/views.py
@@ -6,6 +6,17 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Sm3llin'}
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Melbourne!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avenger\'s movie was so cool!'
+        }
+    ]
     return render_template('index.html',
                            title="Home",
-                           user=user)
+                           user=user,
+                           posts=posts)
